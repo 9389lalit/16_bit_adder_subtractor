32a70d9915dc5d75e9adc771dbf6a63f7fc7e8cc
Lalit singh <99964550+9389lalit@users.noreply.github.com>
2023-06-22 17:25:43 +0530

    Update design_16bit_adder_subtractor.v

--- design_16bit_adder_subtractor.v
+++ design_16bit_adder_subtractor.v
@@ -29,18 +29,20 @@ output reg co);
 
 always @(*)
 begin
-
+// for the sum of 2 numbers 
 if(M==0)
 
 begin
 {co,s}=a+b+16'b0000000000000000;
 end
-
+  
+// for the subtraction of 2 number
 else if(M==1)
 
 begin
 {co,s}=a+(16'b1111111111111111^b)+16'b0000000000000001;
-
+  
+// to get the number in 2's complement
 if(co==0)
 
 begin
